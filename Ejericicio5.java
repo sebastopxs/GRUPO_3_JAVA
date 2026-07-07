e19d60b95ae63715b129f798dc5369eb14456870
Sebastian <jobhuamanrios@gmail.com>
2026-07-07 08:36:56 -0500

    Implement factorial calculation in Ejercicio5

--- Ejericicio5.java
+++ Ejericicio5.java
@@ -1,1 +1,18 @@
+public class Ejercicio5 {
+    public static int factorial(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println("Factorial de " + n + " es: " + factorial(n));
+    }
+}
+
+
+
 

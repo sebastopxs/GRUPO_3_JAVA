bccf48c7895c4c35ecfe6653d1235594432b1b2d
Sebastian <jobhuamanrios@gmail.com>
2026-07-07 08:36:36 -0500

    Update Ejericicio4.java

--- Ejericicio4.java
+++ Ejericicio4.java
@@ -1,1 +1,19 @@
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        int[] vector = {7, 2, 9};
+        int n = vector.length;
+        
+        int max = vector[0];
+        int min = vector[0];
+        
+        for (int i = 1; i < n; i++) {
+            if (vector[i] > max) max = vector[i];
+            if (vector[i] < min) min = vector[i];
+        }
+        
+        System.out.println("Maximo: " + max + ", Minimo: " + min);
+    }
+}
+
+
 

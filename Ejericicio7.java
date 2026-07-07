fa9e86a0002a7e0b03604a6f8a5e9b89de350aac
Sebastian <jobhuamanrios@gmail.com>
2026-07-07 08:37:30 -0500

    Implement insertion sort in Ejercicio7.java

--- Ejericicio7.java
+++ Ejericicio7.java
@@ -1,1 +1,22 @@
+import java.util.Arrays;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        int[] vector = {5, 3, 4};
+        int n = vector.length;
+        
+        for (int i = 1; i < n; i++) {
+            int clave = vector[i];
+            int j = i - 1;
+            
+            while (j >= 0 && vector[j] > clave) {
+                vector[j + 1] = vector[j];
+                j = j - 1;
+            }
+            vector[j + 1] = clave;
+        }
+        
+        System.out.println("Vector ordenado: " + Arrays.toString(vector));
+    }
+}
 
